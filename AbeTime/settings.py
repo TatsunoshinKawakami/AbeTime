7d3ea52c8c00727bb636f7854502d02e923d16bb
tatsunoshin <tatsunoshin.0602@gmail.com>
2023-08-25 15:26:12 +0900

    pwa対応

--- AbeTime/settings.py
+++ AbeTime/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +130,7 @@ AUTH_USER_MODEL = 'User.AbeUser'
 LOGIN_URL = "User:login"
 LOGIN_REDIRECT_URL = "User:index"
 LOGOUT_REDIRECT_URL = "User:login"
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
--- AbeTime/urls.py
+++ AbeTime/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("Manager/", include("Manager.urls")),
     path("", include("User.urls")),
+    path('sw.js', (TemplateView.as_view(template_name="User/sw.js",
+    content_type='application/javascript', )), name='sw.js'),
 ]
